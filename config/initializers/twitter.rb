5793f42dad44a4b56a2f8787964eaf2aa6386f39
Kyle Boe <ksboe1@gmail.com>
2016-05-28 10:31:14 -0700

    added constant for client generation

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,5 +1,5 @@
 require 'twitter'
-Twitter::REST::Client.new do |config|
+CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
   config.access_token = ENV['TWITTER_ACCESS_TOKEN']
